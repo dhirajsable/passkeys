fa6e513511e06b357d38e992ee0e564ff763274d
Dhiraj Sable <sable.dhiraj@gmail.com>
2023-02-10 19:44:08 -0800

    Added project skeleton for passkey implementation.

---
+++ passwordless-auth/src/main/java/com/example/passwordlessauth/PasswordlessAuthApplication.java
@@ -0,0 +1,12 @@
+package com.example.passwordlessauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordlessAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordlessAuthApplication.class, args);
+    }
+}
---
+++ passwordless-auth/src/test/java/com/example/passwordlessauth/PasswordlessAuthApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.passwordlessauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordlessAuthApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
